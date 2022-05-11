f5971ef267781b9329ad1b75716484090b418e7c
Gabriel Farfan <dcnlite@gmail.com>
2022-05-11 13:09:25 -0300

    completed work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -53,15 +53,15 @@ const Work = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Netflick (Netflix Clone)
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://netflick.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/gabriel-farfan/netflick'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
